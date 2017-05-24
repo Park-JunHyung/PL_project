c910ce74afe3a43d1051876207b103e940e5f0d4
Park JunHyung <schwazodiac@gmail.com>
2017-05-24 17:57:40 +0900

    basic lambda function implement

--- Cute17 Interpreter.py
+++ Cute17 Interpreter.py
@@ -532,15 +532,15 @@ def run_func(op_code_node):
     def run_lambda(node):
         l_node = node.value.next
         funcNode = l_node.next
-        newNode=run_search(funcNode.value,l_node.value,node.next)
-        answer=run_expr(newNode)
-        return answer#Node(TokenType.ID, "LAMBDA")
+        newNode=run_search(funcNode.value, l_node.value, node.next)
+        answer = run_expr(Node(TokenType.LIST, newNode))
+        return answer
 
-    def run_search(node,varNode,targetNode):
+    def run_search(node, varNode, targetNode):
         if (node is not None):
             if (node.value is varNode.value):
-                node.next = targetNode
-            run_search(node.next,varNode, targetNode)
+                node.value = targetNode.value
+            run_search(node.next, varNode, targetNode)
         return node
 
     def create_new_quote_list(value_node, list_flag=False):
